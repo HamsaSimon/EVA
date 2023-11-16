1659edf7dbdae5fd40eae454288f0b5cf79f567e
Simon <simon@Hedwigs-iMac2local.local>
2023-11-15 21:12:24 -0500

    first commit for creating eva programming language

---
+++ eva.py
@@ -0,0 +1,13 @@
+
+#Eva interpreter
+class Eva:
+    def __init__(self) -> None:
+        pass
+
+    def eval(self,exp):
+        raise Exception("Unimplemented")
+    
+
+eva = Eva()
+
+assert eva.eval(1) == 1
