a59bf626f431fd670c64ab5d1fc5326abc580496
Simon <simon@hedwigsac2local.home>
2023-11-23 21:29:48 -0500

    initial solution to recursive expressions

--- eva.py
+++ eva.py
@@ -10,6 +10,8 @@ class Eva:
         if self.isString(exp):
             return exp[1:-1]
         if (exp[0] == '+'):
+            if (isinstance(exp[1],list)):
+                exp[1] = self.eval(exp[1])
             return exp[1] + exp[2]
         
         raise Exception("Unimplemented")
@@ -20,6 +22,12 @@ class Eva:
     def isString(self,var):
         # check that it is a string and that the first and last characters are double quotes
         return type(var) == str and var[0] == '''"'''  and var[-1] == '''"'''
+    
+    #def sum(self,exp):
+     #   start = 1
+      #  if (exp[start] == '+'):
+
+
 
     
 eva = Eva()
@@ -27,5 +35,6 @@ eva = Eva()
 assert eva.eval(1) == 1
 assert eva.eval('''"Hello"''') == "Hello"
 assert eva.eval(['+', 1, 5]) == 6
+testList = ['+',['+', 3, 2],5 ]
 assert eva.eval(['+',['+', 3, 2],5 ]) == 10
 
